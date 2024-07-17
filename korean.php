ebac19ec8e145f2ff041eefe7f424cf478ff7fef
zardkim <zardkim@gmail.com>
2024-07-18 08:53:11 +0900

    Add files via upload

---
+++ korean.php
@@ -0,0 +1,24 @@
+<?php
+// The friendly name of the language
+$langinfo['name'] = "한국어";
+
+// The author of the language
+$langinfo['author'] = "zardkim";
+
+// The language authors website
+$langinfo['website'] = "https://";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1838";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "kr";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
